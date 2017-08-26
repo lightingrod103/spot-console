4d1924967de1db5473b9c0352c92a27147ec2bbf
lrod103 <lightingrod103@gmail.com>
2017-08-26 17:27:20 -0400

    Updating master branch with connection logic.

--- SpotConsole/Program.cs
+++ SpotConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,63 @@ namespace SpotConsole
 {
     class Program
     {
+        private static SpotifyLocalAPI localAPI;
         static void Main(string[] args)
         {
+            localAPI = new SpotifyLocalAPI();
+            localAPI.ListenForEvents = true;
+
+            while (!SpotifyLocalAPI.IsSpotifyRunning() || !SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+            {
+                Console.WriteLine("Checking connection readiness...");
+                if (!SpotifyLocalAPI.IsSpotifyRunning())    //Make sure the spotify client is running
+                {
+                    Console.WriteLine("No Spotify instance was found.\nAttempting to start Spotify...");
+                    SpotifyLocalAPI.RunSpotify();
+                    while (!SpotifyLocalAPI.IsSpotifyRunning()) { } //Checks until the process is started.
+                                
+                }
+
+                //System.Threading.Thread.Sleep(3000);    //Wait so connection will pass through.
+
+                if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())   //Make sure the spotify web helper is running
+                {
+                    Console.WriteLine("No Spotify Web Helper instance was found.\nAttempting to start Spotify Web Helper...");
+                    SpotifyLocalAPI.RunSpotifyWebHelper();
+                    while (!SpotifyLocalAPI.IsSpotifyWebHelperRunning()) { }    //Checks until the process is started.
+                }
+            }
+
+
+
+
+            if (!localAPI.Connect())
+            {
+                Console.WriteLine("Unable to connect to Spotify. System will keep trying. Press any key to end.");
+                while (!localAPI.Connect())    //We need to call Connect before fetching infos, this will handle Auth stuff
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("Connection to Spotify successful.");
+                
+                
+           
+
+
+
+
+
+            Console.WriteLine("Press enter to pause spotify.");
+            Console.ReadLine();
+            localAPI.Pause();
+
+            Console.WriteLine("End run.");
+            Console.ReadLine();
         }
     }
 }
